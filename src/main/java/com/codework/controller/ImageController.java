d897e1072845531068cfe12383b05c24fb0574cf
rameshpaswan44 <raj168ramesh@gmail.com>
2025-08-21 21:36:30 +0545

    Ïnital Commit

--- src/main/java/com/codework/controller/ImageController.java
+++ src/main/java/com/codework/controller/ImageController.java
@@ -1,20 +1,25 @@
 package com.codework.controller;
 
 import com.codework.dto.ImageDto;
+import com.codework.exception.ResourceNotFoundException;
+import com.codework.model.Image;
 import com.codework.response.ApiResponse;
 import com.codework.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 @RequestMapping("${api.prefix}/images")
@@ -35,5 +40,44 @@ public class ImageController {
 
         }
 
+    }
+    @GetMapping("/image/download/{imageId}")
+    public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
+    Image image = imageService.getImageById(imageId);
+    ByteArrayResource resource  = new ByteArrayResource(image.getImage().getBytes(1,(int) image.getImage().length()));
+    return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+            .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\""+image.getFileName()+ "\"")
+            .body(resource);
+}
+
+   @PutMapping("/image/{imageId}/update")
+   public ResponseEntity<ApiResponse> updateImage(@PathVariable Long imageId, @RequestBody MultipartFile file){
+
+        try {
+            Image image = imageService.getImageById(imageId);
+            if (image != null){
+                imageService.updateImage(file, imageId);
+                return ResponseEntity.ok(new ApiResponse("update success", null));
+            }
+        }catch (ResourceNotFoundException e) {
+            return  ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null));
+        }
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("update failed..!", INTERNAL_SERVER_ERROR));
+        }
+
+
+    @DeleteMapping("/image/{imageId}/delete")
+    public ResponseEntity<ApiResponse> deleteImage(@PathVariable Long imageId){
+
+        try {
+            Image image = imageService.getImageById(imageId);
+            if (image != null){
+                imageService.deleteImageById(imageId);
+                return ResponseEntity.ok(new ApiResponse("delete success", null));
+            }
+        }catch (ResourceNotFoundException e) {
+            return  ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(),null));
+        }
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("delete failed..!", INTERNAL_SERVER_ERROR));
     }
 }
